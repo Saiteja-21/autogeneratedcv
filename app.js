a56f768387f3fed2f5fcdd7e33e3f6091a1a33ab
Saiteja-21 <saitejakatkam123@gmail.com>
2023-02-04 14:26:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const passingyear=document.getElementById("passingyear");
 const cgpa=document.getElementById("cgpa");
 const skills=document.getElementById("skills");
 const project=document.getElementById("project");
-
+const p_description=document.getElementById("p_description");
 const link=document.getElementById("link");
 const Designation=document.getElementById("Designation");
 const company=document.getElementById("company");
@@ -29,35 +29,53 @@ function handleSubmit(e) {
   e.preventDefault();
   const name_resume = document.getElementById("name_resume");
   name_resume.innerHTML = name.value;
+  document.getElementById("fname").value="";
   document.getElementById("email_resume").innerHTML=email.value;
+  document.getElementById("email").value="";
   const phone_resume = document.getElementById("phone_resume");
   phone_resume.textContent="| ";
   phone_resume.textContent+=phone.value;
+  document.getElementById("phone").value="";
   const github_resume=document.getElementById("github_resume");
   github_resume.textContent="| ";
   github_resume.textContent+=github.value;
+  document.getElementById("github").value="";
   const resume_resume=document.getElementById("linkedin_resume");
   resume_resume.textContent="| ";
   resume_resume.textContent+=linkedin.value;
+  document.getElementById("linkedin").value="";
   document.getElementById("college_name").innerHTML=college.value;
+  document.getElementById("college").value="";
   document.getElementById("degree_resume").innerHTML=degree.value;
+  document.getElementById("degree").value="";
   document.getElementById("startingyear_resume").innerHTML=startingyear.value;
+  document.getElementById("startingyear").value="";
   const passingyear_resume = document.getElementById("passingyear_resume");
   passingyear_resume.textContent="- ";
   passingyear_resume.textContent+=passingyear.value;
+  document.getElementById("passingyear").value="";
   const cgpa_resume=document.getElementById("cgpa_resume");
   cgpa_resume.textContent="CGPA:"
   cgpa_resume.textContent+=cgpa.value;
+  document.getElementById("cgpa").value="";
   document.getElementById("skills_resume").innerHTML=skills.value;
+  document.getElementById("skills").value="";
   document.getElementById("project_resume").innerHTML=project.value;
+  document.getElementById("project").value="";
   document.getElementById("projectdescription_resume").innerHTML=p_description.value;
+  document.getElementById("p_description").value="";
   const link_resume=document.getElementById("link_resume");
   link_resume.textContent="Link-";
   link_resume.textContent+=link.value;
+  document.getElementById("link").value="";
   document.getElementById("designation_resume").innerHTML=Designation.value;
+  document.getElementById("Designation").value="";
   document.getElementById("company_resume").innerHTML=company.value;
+  document.getElementById("company").value="";
   document.getElementById("duration_resume").innerHTML=Duration.value;
+  document.getElementById("Duration").value="";
   document.getElementById("experiencedescription_resume").innerHTML=e_description.value;
+  document.getElementById("e_description").value="";
 
   
 
